5714f47b812b42a9ba8d44c91e1989b9b3aae8bc
nikklaud <titnik47@gmail.com>
2026-02-17 21:56:51 +0500

    create a project

---
+++ src/test/java/org/nikklaud/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nikklaud.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
